a4e6ba0b62f4fc4f81d5c142bf14b61203290986
Ueban1342 <123741444+Ueban1342@users.noreply.github.com>
2026-07-07 17:45:39 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# config.py
+
+# Токен вашего Telegram-бота, полученный у @BotFather
+TELEGRAM_BOT_TOKEN = "8764461762:AAEe5ea9infQNwhY8eCX-WYMbAlGJRqItyA"
+
+# Ваш Telegram ID (администратора/мастера). Узнать можно через бота @userinfobot
+ADMIN_ID = 1867623281
+
+# Пароль для доступа к веб-админке (Streamlit)
+ADMIN_PANEL_PASSWORD = "89063"
+
+# Настройки напоминаний (в часах до записи)
+REMINDER_24H = 24
+REMINDER_2H = 2
+
+# Временная зона (для корректной работы с датами и временем)
+# Например: 'Europe/Moscow'
+TIMEZONE = 'Asia/Yekaterinburg'
